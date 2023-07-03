f03668cf790457da919e712c5d3c3f357738a0c1
zhangwang <cute_zw@163.com>
2023-07-03 15:02:23 +0800

    JMIR 45846

--- S2_forecast.r
+++ S2_forecast.r
@@ -2,6 +2,7 @@ library(fpp3)
 library(latex2exp)
 library(data.table)
 library(knitr)
+
 library(ggpattern)
 
 ## Compare results of different models for 15-day forecasting
@@ -10,11 +11,13 @@ library(ggpattern)
 rm(list = ls(all = TRUE))
 
 # Get hourly visits dataset from CSV file
-vis_data <- readr::read_csv("core/open_hourly_visits.csv") %>% as_tsibble(index = time)
+vis_data <-
+  readr::read_csv("open_hourly_visits.csv") %>% as_tsibble(index = time)
 
 # Divide data into training set and test set
 vis_training <- vis_data %>%
-  filter_index("2020-01-23 00" ~ "2023-04-23 23")   
+  filter_index("2020-01-23 00" ~ "2023-04-23 23")
+
 h_step <- 24 * 15
 
 training_tail <- vis_training %>% tail(1)
@@ -33,11 +36,12 @@ com_fit <- vis_training %>%
     sarima = ARIMA(visits),
     nnetar = NNETAR(visits, MaxNWts = 84581),
     stl_arima = decomposition_model(
-      STL(visits ~
-        season(period = 24) +
-        season(period = 24 * 7) +
-        season(period = 24 * 365.25),
-      robust = TRUE
+      STL(
+        visits ~
+          season(period = 24) +
+          season(period = 24 * 7) +
+          season(period = 24 * 365.25),
+        robust = TRUE
       ),
       ARIMA(season_adjust ~ PDQ(0, 0, 0))
     )
@@ -49,57 +53,118 @@ com_fit <- vis_training %>%
     combination_4 = (nnetar + sarima + stl_arima) / 3
   )
 
+# Get models estimates and summary measures
 com_fit %>% select(snaive) %>% report()
 com_fit %>% select(sarima) %>% report()
 com_fit %>% select(nnetar) %>% report()
 com_fit %>% select(stl_arima) %>% report()
 
+com_fit %>% select(sarima) %>% coef()
+com_fit %>% select(sarima) %>% str()
+com_fit %>% select(stl_arima) %>% str()
+
+# Do the forecasting
 com_fc <- com_fit %>% fabletools::forecast(h = h_step)
 
+# Calculate the accuracy
 com_acc <- fabletools::accuracy(com_fc, vis_data)
 com_acc
 
 ## ---------------------Step 3: Compare results--------------------
 
 # Plot the forecast results
-fig_map <- tibble(m_name = c("snaive","sarima","nnetar","stl_arima","combination_1","combination_2","combination_3","combination_4"),
-  t_name = c("SNaïve","SARIMA","NNAR","STLF","Hybrid SARIMA-STLF","Hybrid NNAR-STLF","Hybrid SARIMA-NNAR","Hybrid SARIMA-NNAR-STLF"))
+fig_map <-
+  tibble(
+    m_name = c(
+      "snaive",
+      "sarima",
+      "nnetar",
+      "stl_arima",
+      "combination_1",
+      "combination_2",
+      "combination_3",
+      "combination_4"
+    ),
+    t_name = c(
+      "SNaïve",
+      "SARIMA",
+      "NNAR",
+      "STLF",
+      "Hybrid SARIMA-STLF",
+      "Hybrid NNAR-STLF",
+      "Hybrid SARIMA-NNAR",
+      "Hybrid SARIMA-NNAR-STLF"
+    )
+  )
 
-for(i in seq(nrow(fig_map))){
-  model_fc <- com_fc %>% filter(.model == fig_map[i,]$m_name) 
+for (i in seq(nrow(fig_map))) {
+  model_fc <- com_fc %>% filter(.model == fig_map[i,]$m_name)
   vis_future$fc_visits <- model_fc$.mean
-
-  pdf(paste("results_",fig_map[i,]$t_name,".pdf", sep=""), width = 7, height = 5)
+  
+  pdf(
+    paste("results_", fig_map[i,]$t_name, ".pdf", sep = ""),
+    width = 7,
+    height = 5
+  )
   ggplot(vis_future, aes(x = time)) +
-  geom_line(aes(y = visits, color = "Observation")) +
-  geom_line(aes(y = fc_visits, color = paste(fig_map[i,]$t_name, "Prediction", sep=" "))) +
-  scale_color_manual('', values=c("#1cdf3cc8","#1d55e1c7"), breaks=c(paste(fig_map[i,]$t_name, "Prediction", sep=" "), "Observation")) +
-  labs(x = 'Hour', y = 'Visits') +
-  theme(legend.position=c(0,1),legend.justification = c("left", "top"),legend.key = element_blank(), legend.title = element_blank(),legend.background = element_rect(fill = "white", colour = "gray", linewidth=0.1),
-  legend.margin=margin(t = -0.2, unit='cm'))
+    geom_line(aes(y = visits, color = "Observation")) +
+    geom_line(aes(
+      y = fc_visits,
+      color = paste(fig_map[i,]$t_name, "Prediction", sep = " ")
+    )) +
+    scale_color_manual(
+      '',
+      values = c("#1cdf3cc8", "#1d55e1c7"),
+      breaks = c(paste(fig_map[i,]$t_name, "Prediction", sep = " "), "Observation")
+    ) +
+    labs(x = 'Hour', y = 'Visits') +
+    theme(
+      legend.position = c(0, 1),
+      legend.justification = c("left", "top"),
+      legend.key = element_blank(),
+      legend.title = element_blank(),
+      legend.background = element_rect(
+        fill = "white",
+        colour = "gray",
+        linewidth = 0.1
+      ),
+      legend.margin = margin(t = -0.2, unit = 'cm')
+    )
   dev.off()
-
+  
   Sys.sleep(3)
 }
 
 # Accuracy metrics for all models
 # RMSE, MAE
-acc_data <- com_acc %>% mutate(model = recode(.model,
-  snaive = "SNaïve", sarima = "SARIMA", stl_arima = "STLF",
-  nnetar = "NNAR",
-  combination_1 = "Hybrid SARIMA-STLF", combination_2 = "Hybrid NNAR-STLF",
-  combination_3 = "Hybrid SARIMA-NNAR", combination_4 = "Hybrid SARIMA-NNAR-STLF"
-)) %>% select(.model, model, RMSE, MAE)  
-    
+acc_data <- com_acc %>% mutate(
+  model = recode(
+    .model,
+    snaive = "SNaïve",
+    sarima = "SARIMA",
+    stl_arima = "STLF",
+    nnetar = "NNAR",
+    combination_1 = "Hybrid SARIMA-STLF",
+    combination_2 = "Hybrid NNAR-STLF",
+    combination_3 = "Hybrid SARIMA-NNAR",
+    combination_4 = "Hybrid SARIMA-NNAR-STLF"
+  )
+) %>% select(.model, model, RMSE, MAE)
+
 acc_data %>% arrange(RMSE)
 acc_data %>% arrange(MAE)
 
 tiff(
-  filename = "acc_bar.tiff", width = 20, height = 15,
-  units = "cm", res = 300
+  filename = "acc_bar.tiff",
+  width = 20,
+  height = 15,
+  units = "cm",
+  res = 300
 )
 acc_data %>%
-  pivot_longer(cols = RMSE:MAE, names_to = "measure", values_to = "value") %>%
+  pivot_longer(cols = RMSE:MAE,
+               names_to = "measure",
+               values_to = "value") %>%
   ggplot(mapping = aes(x = .model, y = value, fill = model)) +
   geom_col_pattern(
     aes(pattern_type = model),
@@ -111,20 +176,39 @@ acc_data %>%
     linewidth = 0.05,
     pattern_density = 2
   ) +
-  scale_pattern_type_discrete(choices = c("gray50", "hs_bdiagonal", "hs_horizontal", "hs_diagcross", "hs_vertical", "gray100", "hs_fdiagonal", "gray0")) +
-  facet_wrap(~`measure`) +
+  scale_pattern_type_discrete(
+    choices = c(
+      "gray50",
+      "hs_bdiagonal",
+      "hs_horizontal",
+      "hs_diagcross",
+      "hs_vertical",
+      "gray100",
+      "hs_fdiagonal",
+      "gray0"
+    )
+  ) +
+  facet_wrap(~ `measure`) +
   theme(
-    axis.text.x = element_blank(), axis.title.x = element_blank(), axis.ticks.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_blank(),
     axis.title.y = element_blank()
   ) +
-  geom_text(aes(label = round(value, 2)), vjust = 0, color = "black", size = 2.8) +
+  geom_text(
+    aes(label = round(value, 2)),
+    vjust = 0,
+    color = "black",
+    size = 2.8
+  ) +
   theme(aspect.ratio = 1) +
   theme(
     legend.position = "bottom",
     legend.title = element_blank(),
     legend.margin = margin(0, 0, 15, 0, "pt"),
     legend.box.margin = margin(0, 0, 0, 0, "pt"),
-    legend.key.size = unit(20, "pt"), legend.text = element_text(size = 9)
+    legend.key.size = unit(20, "pt"),
+    legend.text = element_text(size = 9)
   ) +
   theme(plot.margin = unit(c(10, 0, 0, 0), "pt"))
 dev.off()
--- S3_cross_validation.r
+++ S3_cross_validation.r
@@ -10,15 +10,16 @@ library(ggpubr)
 rm(list = ls(all = TRUE))
 
 # Get hourly visits dataset from CSV file
-vis_data <- readr::read_csv("core/open_hourly_visits.csv") %>% as_tsibble(index = time)
+vis_data <-
+  readr::read_csv("open_hourly_visits.csv") %>% as_tsibble(index = time)
 
 # Divide training sets and test sets of cross-validation
-rl_init <- 24 * 827 
+rl_init <- 24 * 827
 rl_step <- 24 * 30
 h_step <- 24 * 30
 
 vis_training <- vis_data %>%
-  filter_index("2020-01-23 00" ~ "2023-04-23 23") %>%       
+  filter_index("2020-01-23 00" ~ "2023-04-23 23") %>%
   stretch_tsibble(.init = rl_init, .step = rl_step)
 
 ## ---------------------Step 2: Forecasting--------------------
@@ -31,11 +32,12 @@ com_fit <- vis_training %>%
     sarima = ARIMA(visits),
     nnetar = NNETAR(visits, MaxNWts = 84581),
     stl_arima = decomposition_model(
-      STL(visits ~
-        season(period = 24) +
-        season(period = 24 * 7) +
-        season(period = 24 * 365.25),
-      robust = TRUE
+      STL(
+        visits ~
+          season(period = 24) +
+          season(period = 24 * 7) +
+          season(period = 24 * 365.25),
+        robust = TRUE
       ),
       ARIMA(season_adjust ~ PDQ(0, 0, 0))
     )
@@ -47,8 +49,10 @@ com_fit <- vis_training %>%
     combination_4 = (nnetar + sarima + stl_arima) / 3
   )
 
+# Do the forecasting
 com_fc <- com_fit %>% fabletools::forecast(h = h_step)
 
+# Calculate the accuracy
 com_acc <- fabletools::accuracy(com_fc, vis_data)
 com_acc
 
@@ -62,35 +66,55 @@ fc_acc_by_model <- function(model_name) {
     mutate(h = row_number() %/% 24 + 1) %>%
     ungroup() %>%
     as_fable(response = "visits", distribution = visits)
-
+  
   return(fc %>%
-    fabletools::accuracy(vis_data, by = c("h", ".model")) %>%
-    filter(h < 31))
+           fabletools::accuracy(vis_data, by = c("h", ".model")) %>%
+           filter(h < 31))
 }
 
-model_names <- c("combination_1","combination_2","combination_3",
-"combination_4","stl_arima","snaive","sarima","nnetar")
+model_names <- c(
+  "combination_1",
+  "combination_2",
+  "combination_3",
+  "combination_4",
+  "stl_arima",
+  "snaive",
+  "sarima",
+  "nnetar"
+)
 
-for(m in model_names){
-  if(exists("horizon_acc")){
+for (m in model_names) {
+  if (exists("horizon_acc")) {
     horizon_acc <- horizon_acc %>% add_row(fc_acc_by_model(m))
-  }else {
+  } else {
     horizon_acc <- fc_acc_by_model(m)
   }
 }
 
 horizon_acc <- horizon_acc %>%
-  mutate(model = recode(.model,
-    snaive = "SNaïve", sarima = "SARIMA", stl_arima = "STLF",
-    nnetar = "NNAR",
-    combination_1 = "Hybrid SARIMA-STLF", combination_2 = "Hybrid NNAR-STLF",
-    combination_3 = "Hybrid SARIMA-NNAR", combination_4 = "Hybrid SARIMA-NNAR-STLF"
-  ))
+  mutate(
+    model = recode(
+      .model,
+      snaive = "SNaïve",
+      sarima = "SARIMA",
+      stl_arima = "STLF",
+      nnetar = "NNAR",
+      combination_1 = "Hybrid SARIMA-STLF",
+      combination_2 = "Hybrid NNAR-STLF",
+      combination_3 = "Hybrid SARIMA-NNAR",
+      combination_4 = "Hybrid SARIMA-NNAR-STLF"
+    )
+  )
 
-# Plot MAE for forecast horizon accuracy 
+# Plot MAE for forecast horizon accuracy
 pdf("cv_mae.pdf")
 horizon_acc %>%
-  ggplot(aes(x = h, y = MAE, colour = model, shape = model)) +
+  ggplot(aes(
+    x = h,
+    y = MAE,
+    colour = model,
+    shape = model
+  )) +
   geom_point(size = 2.5) +
   xlab("Forecast horizon (day)") +
   theme(
@@ -98,16 +122,22 @@ horizon_acc %>%
     legend.title = element_blank(),
     legend.margin = margin(0, 0, 10, 0, "pt"),
     legend.box.margin = margin(0, 0, 0, 0, "pt"),
-    legend.key.size = unit(15, "pt"), legend.text = element_text(size = 8)
+    legend.key.size = unit(15, "pt"),
+    legend.text = element_text(size = 8)
   ) +
   theme(plot.margin = unit(c(20, 20, 10, 10), "pt")) +
   scale_shape_manual(values = c(17, 2, 17, 2, 3, 20, 3, 20))
 dev.off()
 
-# Plot RMSE for forecast horizon accuracy 
+# Plot RMSE for forecast horizon accuracy
 pdf("cv_rmse.pdf")
 horizon_acc %>%
-  ggplot(aes(x = h, y = RMSE, colour = model, shape = model)) +
+  ggplot(aes(
+    x = h,
+    y = RMSE,
+    colour = model,
+    shape = model
+  )) +
   geom_point(size = 2.5) +
   xlab("Forecast horizon (day)") +
   theme(
@@ -115,7 +145,8 @@ horizon_acc %>%
     legend.title = element_blank(),
     legend.margin = margin(0, 0, 10, 0, "pt"),
     legend.box.margin = margin(0, 0, 0, 0, "pt"),
-    legend.key.size = unit(15, "pt"), legend.text = element_text(size = 8)
+    legend.key.size = unit(15, "pt"),
+    legend.text = element_text(size = 8)
   ) +
   theme(plot.margin = unit(c(20, 20, 10, 10), "pt")) +
   scale_shape_manual(values = c(17, 2, 17, 2, 3, 20, 3, 20))
@@ -128,23 +159,27 @@ month_acc <- com_fc %>%
   fabletools::accuracy(vis_data, by = c("m", ".model"))
 
 # Plot boxplot
-box_data <- data.frame(
-  month = month_acc$m, RMSE = month_acc$RMSE, MAE = month_acc$MAE
-)
+box_data <- data.frame(month = month_acc$m,
+                       RMSE = month_acc$RMSE,
+                       MAE = month_acc$MAE)
 
 pdf("month_rmse_boxplot.pdf")
-boxplot(box_data$RMSE ~ box_data$m,
+boxplot(
+  box_data$RMSE ~ box_data$m,
   boxwex = 0.8,
   col = rgb(0.2, 0.3, 0.8, 0.5),
-  ylab = "RMSE", xlab = "Month"
+  ylab = "RMSE",
+  xlab = "Month"
 )
 dev.off()
 
 pdf("month_mae_boxplot.pdf")
-boxplot(box_data$MAE ~ box_data$m,
+boxplot(
+  box_data$MAE ~ box_data$m,
   boxwex = 0.8,
   col = rgb(0.2, 0.3, 0.8, 0.5),
-  ylab = "MAE", xlab = "Month"
+  ylab = "MAE",
+  xlab = "Month"
 )
 dev.off()
 
@@ -156,25 +191,32 @@ acc_of_consequent_days <- function(model_name, h_day) {
     mutate(h = row_number() %/% 24 + 1) %>%
     ungroup() %>%
     as_fable(response = "visits", distribution = visits)
-
+  
   return(fc %>% filter(h <= h_day) %>%
-    fabletools::accuracy(vis_data))
+           fabletools::accuracy(vis_data))
 }
 
-for(m in model_names){
-  if(exists("acc_15_days")){
-    acc_15_days <- acc_15_days %>% add_row(acc_of_consequent_days(m, 15))
-  }else {
+for (m in model_names) {
+  if (exists("acc_15_days")) {
+    acc_15_days <-
+      acc_15_days %>% add_row(acc_of_consequent_days(m, 15))
+  } else {
     acc_15_days <- acc_of_consequent_days(m, 15)
   }
 }
 
 # Accuracy metrics results for the next 15 days
-acc_15_days %>%  
-  mutate(model = recode(.model,
-    snaive = "SNaïve", sarima = "SARIMA", stl_arima = "STLF",
-    nnetar = "NNAR",
-    combination_1 = "Hybrid SARIMA-STLF", combination_2 = "Hybrid NNAR-STLF",
-    combination_3 = "Hybrid SARIMA-NNAR", combination_4 = "Hybrid SARIMA-NNAR-STLF"
-  )) %>% select(.model, model, MAE, RMSE) %>% arrange(RMSE)
-
+acc_15_days %>%
+  mutate(
+    model = recode(
+      .model,
+      snaive = "SNaïve",
+      sarima = "SARIMA",
+      stl_arima = "STLF",
+      nnetar = "NNAR",
+      combination_1 = "Hybrid SARIMA-STLF",
+      combination_2 = "Hybrid NNAR-STLF",
+      combination_3 = "Hybrid SARIMA-NNAR",
+      combination_4 = "Hybrid SARIMA-NNAR-STLF"
+    )
+  ) %>% select(.model, model, MAE, RMSE) %>% arrange(RMSE)
